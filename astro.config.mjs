50539605998d63b722259842bb6f0f6bba61de6a
JianchengWang <992636536@qq.com>
2026-07-04 12:01:04 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://Goodenough886.github.io', // 换成你的网址
 	integrations: [
 		starlight({
 			title: 'My Docs',
